b1c35ab35b55f620e0043f16741ba76f4cf34478
Alonso Heynes <alonso.heynes@gmail.com>
2019-04-11 17:31:42 -0500

    Complete show and hide functions using buttons. Styling started.

--- main.js
+++ main.js
@@ -5,155 +5,128 @@ let myPicturesArray = [
 		"albumId": 1,
 		"id": 1,
 		"title": "accusamus beatae ad facilis cum similique qui sunt",
-		"url": "http://placehold.it/600/92c952",
-		"thumbnailUrl": "http://placehold.it/150/92c952"
+		"url": "http://placehold.it/600/6927ff",
+		"thumbnailUrl": "http://placehold.it/150/d52484"
 	},
 	{
 		"albumId": 1,
 		"id": 2,
 		"title": "reprehenderit est deserunt velit ipsam",
-		"url": "http://placehold.it/600/771796",
-		"thumbnailUrl": "http://placehold.it/150/771796"
+		"url": "http://placehold.it/600/837dff",
+		"thumbnailUrl": "http://placehold.it/150/cdeeaa"
 	},
 	{
 		"albumId": 2,
 		"id": 51,
 		"title": "non sunt voluptatem placeat consequuntur rem incidunt",
-		"url": "http://placehold.it/600/8e973b",
-		"thumbnailUrl": "http://placehold.it/150/8e973b"
+		"url": "http://placehold.it/600/bf81ff",
+		"thumbnailUrl": "http://placehold.it/150/ff6bd6"
 	},
 	{
 		"albumId": 2,
 		"id": 52,
 		"title": "eveniet pariatur quia nobis reiciendis laboriosam ea",
-		"url": "http://placehold.it/600/121fa4",
-		"thumbnailUrl": "http://placehold.it/150/121fa4"
+		"url": "http://placehold.it/600/ffd581",
+		"thumbnailUrl": "http://placehold.it/150/acdbdf"
 	},
 	{
 		"albumId": 3,
 		"id": 127,
 		"title": "magnam quia sed aspernatur",
-		"url": "http://placehold.it/600/74456b",
-		"thumbnailUrl": "http://placehold.it/150/74456b"
+		"url": "http://placehold.it/600/f8a978",
+		"thumbnailUrl": "http://placehold.it/150/51e3d4"
 	},
 	{
 		"albumId": 3,
 		"id": 128,
 		"title": "est facere ut nam repellat numquam quia quia eos",
 		"url": "http://placehold.it/600/b0931d",
-		"thumbnailUrl": "http://placehold.it/150/b0931d"
+		"thumbnailUrl": "http://placehold.it/150/ffba5a"
 	},
 	{
 		"albumId": 2,
 		"id": 129,
 		"title": "reprehenderit est deserunt velit ipsam",
-		"url": "http://placehold.it/600/771796",
-		"thumbnailUrl": "http://placehold.it/150/771796"
+		"url": "http://placehold.it/600/f1eb9a",
+		"thumbnailUrl": "http://placehold.it/150/23a393"
 	},
 	{
 		"albumId": 1,
 		"id": 42,
 		"title": "reprehenderit est deserunt velit ipsam",
 		"url": "http://placehold.it/600/771796",
-		"thumbnailUrl": "http://placehold.it/150/771796"
+		"thumbnailUrl": "http://placehold.it/150/6d70c6"
 	},
 	{
 		"albumId": 2,
 		"id": 81,
 		"title": "eveniet pariatur quia nobis reiciendis laboriosam ea",
 		"url": "http://placehold.it/600/121fa4",
-		"thumbnailUrl": "http://placehold.it/150/121fa4"
+		"thumbnailUrl": "http://placehold.it/150/96dae4"
 	},
 	{
 		"albumId": 2,
 		"id": 17,
 		"title": "non sunt voluptatem placeat consequuntur rem incidunt",
 		"url": "http://placehold.it/600/8e973b",
-		"thumbnailUrl": "http://placehold.it/150/8e973b"
+		"thumbnailUrl": "http://placehold.it/150/c56868"
 	},
 	{
 		"albumId": 3,
 		"id": 26,
 		"title": "est facere ut nam repellat numquam quia quia eos",
 		"url": "http://placehold.it/600/b0931d",
-		"thumbnailUrl": "http://placehold.it/150/b0931d"
+		"thumbnailUrl": "http://placehold.it/150/ffedc6"
 	}
 ];
 
-// Display picures
-myPicturesArray.forEach(function(currentImage) {
-	let image = document.createElement('img');
-	image.src = currentImage.thumbnailUrl;
-	image.alt = currentImage.title;
-	image.className = 'myImage';         //crear una clase "picture" y añadírsela a cada elemento creado
-	document.querySelector('.sectionGallery').appendChild(image);         //que cada img sea creada dentro del div con la clase "pic"
-	image.onclick = function() {
-		var expandImg = document.querySelector('#expandedImg');
-		expandImg.src = image.src;
-		expandImg.parentElement.style.display = 'block';
-	}
-});
-
-document.querySelector('#buttonAlbum1').addEventListener('click', function(){
-	myPicturesArray.forEach(function(currentImage){
-		if(currentImage.albumId === 1){
+// Display pictures
+function displayAllPictures(){
+	myPicturesArray.forEach(function(currentImage) {
 		let image = document.createElement('img');
 		image.src = currentImage.thumbnailUrl;
 		image.alt = currentImage.title;
-		image.className = 'myImage';         //crear una clase "picture" y añadírsela a cada elemento creado
-		document.querySelector('.sectionGallery').appendChild(image);         //que cada img sea creada dentro del div con la clase "pic"
-		image.onclick = function() {
-			var expandImg = document.querySelector('#expandedImg');
-			expandImg.src = image.src;
-			expandImg.parentElement.style.display = 'block';
-		}
-		}
-	});
-});
+		image.className = 'myImage' + ' ' + 'show album' + currentImage.albumId;         //crear una clase "picture" y añadírsela a cada elemento creado
+		document.querySelector('.images-container').appendChild(image);
 
-document.querySelector('#buttonAlbum2').addEventListener('click', function(){
-	myPicturesArray.forEach(function(currentImage){
-		if(currentImage.albumId === 2){
-		let image = document.createElement('img');
-		image.src = currentImage.thumbnailUrl;
-		image.alt = currentImage.title;
-		image.className = 'myImage';         //crear una clase "picture" y añadírsela a cada elemento creado
-		document.querySelector('.sectionGallery').appendChild(image);         //que cada img sea creada dentro del div con la clase "pic"
-		image.onclick = function() {
-			var expandImg = document.querySelector('#expandedImg');
-			expandImg.src = image.src;
-			expandImg.parentElement.style.display = 'block';
-		}
-		}
-	});
-});
+	})
+}
+displayAllPictures();
 
-document.querySelector('#buttonAlbum3').addEventListener('click', function(){
-	myPicturesArray.forEach(function(currentImage){
-		if(currentImage.albumId === 3){
-		let image = document.createElement('img');
-		image.src = currentImage.thumbnailUrl;
-		image.alt = currentImage.title;
-		image.className = 'myImage';         //crear una clase "picture" y añadírsela a cada elemento creado
-		document.querySelector('.sectionGallery').appendChild(image);         //que cada img sea creada dentro del div con la clase "pic"
-		image.onclick = function() {
-			var expandImg = document.querySelector('#expandedImg');
-			expandImg.src = image.src;
-			expandImg.parentElement.style.display = 'block';
-		}
-		}
-	});
-});
+var btn1 = document.querySelector('.btn1');
 
+btn1.addEventListener('click', displayAlbum1);
 
-/*
-document.querySelector('#buttonAlbum1').addEventListener('click', function(){
-	myPicturesArray.forEach(function(currentImage){
-		let album = currentImage.albumId;
-		if(album !== 1){
-			this.style.display = 'hide';
-		}
+function displayAlbum1(){
+	$('.album1').addClass('show');
+	$('.album2').removeClass('show').addClass('hide');
+	$('.album3').removeClass('show').addClass('hide');
+}
 
-	});
-});
-*/
+var btn2 = document.querySelector('.btn2');
+
+btn2.addEventListener('click', displayAlbum2);
+
+function displayAlbum2(){
+	$('.album1').removeClass('show').addClass('hide');
+	$('.album2').addClass('show');
+	$('.album3').removeClass('show').addClass('hide');
+}
+
+var btn3 = document.querySelector('.btn3');
+
+btn3.addEventListener('click', displayAlbum3);
+
+function displayAlbum3(){
+	$('.album1').removeClass('show').addClass('hide');
+	$('.album2').removeClass('show').addClass('hide');
+	$('.album3').addClass('show');
+}
+
+var btnAllPictures = document.querySelector('.all-pictures');
+
+btnAllPictures.addEventListener('click', displayAllAlbums);
+
+function displayAllAlbums(){
+	$('.myImage').addClass('show').removeClass('hide');
+}
